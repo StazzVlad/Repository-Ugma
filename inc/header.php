52448ec13a33ed3c11fb022d539ab23e0703cc89
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-10-26 00:28:39 -0400

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,51 @@
+    <head>
+        <title>UGMA</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1, minimun-scale=1">
+         <link rel="stylesheet" href="./css/estilos.css">
+
+         <link rel="stylesheet" 
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" 
+         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+         crossorigin="anonymous" 
+         referrerpolicy="no-referrer" />
+
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+    </head>
+    <body>
+        <header>
+            <nav class="nav-top">
+                <img src="./img/logo.png" id="logo">
+                <div class="search-bar">
+                        <input type="text" placeholder="Buscar tesis">
+                        <i class="fa-solid fa-magnifying-glass" id="lupa"></i>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Sobre UGMA</li>
+                        <li>Trabajos</li>
+                        <li>Facultades</li>
+                        <li>Carreras</li>
+                    </ul>
+                </div>
+                <div class="enlaces">
+                    <div>
+                        <a href="" id="uno">Estudia con nosotros</a>
+                    </div>
+                    <div>
+                        <br>
+                        <a href="" id="dos">Acceder</a>
+                        <a href="" id="tres">Registrarse</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            
+        </main>
+    </body>
