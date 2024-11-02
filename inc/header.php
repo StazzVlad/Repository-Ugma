6e6ddf8c05f72d1b0a6871ce301bc4592cd9be78
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-11-02 14:20:37 -0400

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -1,51 +1,92 @@
-    <head>
-        <title>UGMA</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,
-         user-scalable=no, initial-scale=1, maximum-scale=1, minimun-scale=1">
-         <link rel="stylesheet" href="./css/estilos.css">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-         <link rel="stylesheet" 
+    <!--    Iconos      -->
+    <link rel="stylesheet" 
          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" 
          integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
          crossorigin="anonymous" 
-         referrerpolicy="no-referrer" />
+         referrerpolicy="no-referrer" 
+    />
 
-         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    
-    </head>
-    <body>
-        <header>
-            <nav class="nav-top">
-                <img src="./img/logo.png" id="logo">
-                <div class="search-bar">
-                        <input type="text" placeholder="Buscar tesis">
-                        <i class="fa-solid fa-magnifying-glass" id="lupa"></i>
+    <link rel="stylesheet" href="./css/estilos-header.css">
+
+    <title>HEADER</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+                
+            <div class="logo">
+                <img src="./img/UGMA-logo.png" alt="Logo">
+                <span>Universidad Noriental <br> "Gran Mariscal de Ayacucho"</span>
+            </div>
+
+            <nav>
+                <div class="buscador">
+                    <input type="search" placeholder="Buscar">
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
-                <div class="menu">
-                    <ul>
-                        <li>Inicio</li>
-                        <li>Sobre UGMA</li>
-                        <li>Trabajos</li>
-                        <li>Facultades</li>
-                        <li>Carreras</li>
-                    </ul>
-                </div>
-                <div class="enlaces">
-                    <div>
-                        <a href="" id="uno">Estudia con nosotros</a>
-                    </div>
-                    <div>
-                        <br>
-                        <a href="" id="dos">Acceder</a>
-                        <a href="" id="tres">Registrarse</a>
-                    </div>
-                </div>
+
+                <ul class="bar">
+                    <li><a href="#" class="active"><i class="fa-solid fa-house"></i></a></li>
+
+                    <li><a href="#"><i class="fa-solid fa-book-open"></i> Sobre UGMA</a></li>
+
+                    <li class="has-dropdown">
+                        <a href="#"><i class="fa-solid fa-briefcase"></i> Trabajos <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown">
+                            <li><a href="#">Tesis pregrado</a></li>
+                            <li><a href="#">Tesis posgrado</a></li>
+                            <li><a href="#">Pasantías</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-dropdown">
+                        <a href="#"><i class="fa-solid fa-landmark"></i> Facultades <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown">
+                            <li><a href="#">Ingeniería</a></li>
+                            <li><a href="#">Derecho</a></li>
+                            <li><a href="#">Psicología</a></li>
+                            <li><a href="#">FACES</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-dropdown">
+                        <a href="#"><i class="fa-solid fa-graduation-cap"></i> Carreras <i class="fa-solid fa-chevron-down"></i></a>
+                        <ul class="dropdown">
+                            <li><a href="#">Sistemas</a></li>
+                            <li><a href="#">Informática</a></li>
+                            <li><a href="#">Civil</a></li>
+                            <li><a href="">Administración</a></li>
+                            <li><a href="">Derecho</a></li>
+                            <li><a href="">Psicología</a></li>
+                        </ul>
+                    </li>
+
+
+                    
+                </ul>
             </nav>
-        </header>
-        <main>
-            
-        </main>
-    </body>
+
+            <div class="enlaces-header">
+                <div class="ugma">
+                    <button><a href=""><i class="fa-solid fa-chalkboard-user"></i>Estudia con nosotros</a></button>
+                    <button><a href=""><i class="fa-brands fa-facebook only"></i></a></button>
+                    <button><a href=""><i class="fa-brands fa-instagram only"></i></a></button>
+                </div>  
+
+                <div class="usuario">
+                    <button><a href=""> <i class="fa-solid fa-user"></i>Registrarse</a></button>
+                    <button><a href=""><i class="fa-solid fa-glasses"></i>Acceder</a></button>    
+                    <button><a href=""><i class="fa-solid fa-heart only"></i></a></button>
+                </div>
+            </div>
+        </div>
+
+    </header>
+</body>
+</html>
