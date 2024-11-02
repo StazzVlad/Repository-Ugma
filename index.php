730eb25e970642bfec50368a9207707b2ceb01d2
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-11-02 14:12:33 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php
-    require("./inc/session_start.php");
-    include("./inc/header.php");
-    include("./vistas/new_trabajo.php");
+<?php require("./inc/session_start.php");?>
 
-?>
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+    </head>
+    <?php 
+
+
+        //---Logica para manejo de vistas---///
+    //-Entrar a home si no esta en una pagina definida
+    if(!isset($_GET['vista']) || $_GET['vista']==""){
+        $_GET['vista']="inicio";
+    }
+
+    //-Si esta en una pagina distinta al login y 404: Agregar nadbar y script-//
+    if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+        include("./inc/header.php");
+        include("./vistas/".$_GET['vista'].".php"); 
+    }else{
+        if($_GET['vista']=="login"){
+            include("./vistas/login.php");
+        }else{
+            include("./vistas/404.php");
+        }
+    } 
+    ?>
