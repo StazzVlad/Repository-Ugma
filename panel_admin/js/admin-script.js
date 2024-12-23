9cf4e5f6d9d1c1ca8c9d676e52de0c7daebf8d25
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-12-23 13:16:17 -0400

    Add files via upload

--- panel_admin/js/admin-script.js
+++ panel_admin/js/admin-script.js
@@ -85,3 +85,48 @@ allProgress.forEach(item=> {
     item.style.setProperty('--value', item.dataset.value)
 })
 
+// ✅ MAIN CONTENT CLICKS
+document.querySelectorAll('.side-menu a').forEach(item => {
+    item.addEventListener('click', event => {
+        event.preventDefault(); // Evita que el enlace navegue
+        const contentId = item.getAttribute('id'); // Obtiene el id del enlace
+        loadContent(contentId); // Llama a la función para cargar el contenido
+    });
+});
+
+function loadContent(contentId) {
+    const mainContent = document.getElementById('main-content'); // Asegúrate de que el id sea correcto
+    let url = '';
+
+    switch(contentId) {
+        case 'panel':
+            url = '/Repository/panel_admin/vistas/panel.php'; // Ruta al archivo PHP
+            break;
+        case 'pregrado':
+            url = '/Repository/panel_admin/vistas/pregrado.php'; // Cambia esto por la ruta correcta si existe
+            break;
+        case 'proxi':
+            url = '/Repository/panel_admin/vistas/proximamente.php'; // Caso para cuando una pagina aun no este disponible
+            break;
+        // Agrega más casos según sea necesario
+        default:
+            mainContent.innerHTML = '<h2>Contenido por defecto</h2>';
+            return; // Salir de la función si no hay URL
+    }
+
+    // Cargar el contenido del archivo PHP
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la carga del contenido');
+            }
+            return response.text() // Obtener el contenido como texto
+        })
+        .then(data => {
+            mainContent.innerHTML = data; // Insertar el contenido en el main
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            mainContent.innerHTML = '<h2>Error al cargar contenido</h2>'; // Mensaje de error
+        });
+}
